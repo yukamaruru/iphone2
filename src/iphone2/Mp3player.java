0fe79a66227d50b69d67a10c14ab0f1c548edf0a
yukamaru <yuun.vv07@gmail.com>
2025-06-06 21:21:07 +0900

    抽象メソッド

--- src/iphone2/Mp3player.java
+++ src/iphone2/Mp3player.java
@@ -1,5 +1,13 @@
 package iphone2;
 
 public interface Mp3player {
+	//これからルール作りますよという宣言。
+	public abstract void play();
+	public abstract void stop();
+	public abstract void next();
+	public abstract void back();
+	//interfaceとabstractはセット。
+	//メソッド名のみで処理は書かないことを「抽象メソッド」という。
+	//具体的な処理を書かないからabstract(抽象的な)というワードを使う。
 
 }
