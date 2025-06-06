970cf3a745a6a38d853165caf6221ddfb312152f
yukamaru <yuun.vv07@gmail.com>
2025-06-06 21:50:03 +0900

    Smartphoneクラス作成

---
+++ src/iphone2/Iphone.java
@@ -0,0 +1,10 @@
+package iphone2;
+
+public class Iphone {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/iphone2/Smarphone.java
@@ -0,0 +1,20 @@
+package iphone2;
+
+public class Smarphone implements Mp3player{
+	//クラスにインターフェイスを実装するにはimplementsを指定する。
+	//またMp3playerで作ったルール（メソッド）は必ずsmartphoneクラスで
+	//上書きし、処理内容を記述する必要がある。
+	public void play() {
+		System.out.println("再生");
+	}
+	public void stop() {
+		System.out.println("停止");
+	}
+	public void next() {
+		System.out.println("次へ");
+	}
+	public void back() {
+		System.out.println("戻る");
+	}
+
+}
